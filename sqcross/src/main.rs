1e78ac4212d5f7d50b4e3e5b01320d672daeb4fd
bacem <bacem.bergaoui@gmail.com>
2022-04-11 21:08:12 +0100

    first check in

---
+++ sqcross/src/main.rs
@@ -0,0 +1,6 @@
+use sqoinlib::call_lib_func;
+
+fn main() {
+    println!("Starting binary");
+    call_lib_func();
+}
---
+++ sqcrosslib/src/lib.rs
@@ -0,0 +1,8 @@
+
+
+
+
+
+pub fn call_lib_func() {
+    println!("this function was called from a library");
+}
